c610e41ceef737dea50f04744251c14b9c9a72a0
Guilherme Agostinelli <agostinelli.guilherme@gmail.com>
2022-02-22 15:01:06 -0300

    Fix button text overflow.

--- lib/dialog/multi_select_dialog_field.dart
+++ lib/dialog/multi_select_dialog_field.dart
@@ -413,7 +413,9 @@ class __MultiSelectDialogFieldViewState<V>
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                widget.buttonText ?? Text("Select"),
+                Flexible(
+                  child: widget.buttonText ?? Text("Select"),
+                ),
                 widget.buttonIcon ?? Icon(Icons.arrow_downward),
               ],
             ),
